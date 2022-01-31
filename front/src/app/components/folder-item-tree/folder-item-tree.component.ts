b8b973f2c385e156c8397698c1285d3ec6ff761e
toni <amvaratoni@gmail.com>
2022-01-31 17:07:09 +0000

    added folder hierarchy persistance, carefull when mergin this commit

--- front/src/app/components/folder-item-tree/folder-item-tree.component.ts
+++ front/src/app/components/folder-item-tree/folder-item-tree.component.ts
@@ -5,7 +5,7 @@
  *
  * @author: dph000
  */
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Dispatch } from '@ngxs-labs/dispatch-decorator';
 import { Select, Store } from '@ngxs/store';
 import { CustomSelectors } from '@others/custom-selectors';
@@ -22,9 +22,18 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class FolderItemTreeComponent implements OnInit {
 
-  constructor(
-    private _store: Store
-  ) { }
+  // stores state for each folder in hierarchy
+  folderState = {};
+
+
+  constructor(private _store: Store) {
+    // get folder hierarchy state from localstorage, in case it is users first time entering, default department´s state will be set to false(closed)
+    // if localstorage is empty, then set default values
+    this.folderState =
+      JSON.parse(localStorage.getItem('co_folderState'))
+      ||
+      { Default: { open: false }, comment: "This object stores the state of whole folder hierarchy in localstorage" };
+  }
 
   @Input() folder: Folder;
   @Input() level: number;
@@ -40,19 +49,18 @@ export class FolderItemTreeComponent implements OnInit {
 
   // NgOnInit
   ngOnInit() {
-
     // small change as per #3358,  check the modification reasoning in redmine
     if (this.folder.folder_id === 0) {
-      this.expanded$ = new BehaviorSubject<boolean>(false);
-    } else {
       this.expanded$ = new BehaviorSubject<boolean>(true);
+    } else {
+      this.expanded$ = new BehaviorSubject<boolean>(false);
     }
     const isFolderInRoute = this._store.selectSnapshot(CustomSelectors.IsFolderInRoute(this.folder));
     if (isFolderInRoute) {
       this.expanded$.next(true);
     }
-    console.log(this.folder);
   }
+
 
   /**
    * Dispatch functions
@@ -99,22 +107,56 @@ export class FolderItemTreeComponent implements OnInit {
    * Function to toggle expanded state of current folder
    * @param canClose If the current folder should close
    */
-  toggleRow(canClose: boolean) {
+
+  // strange behavior, ask why limit toggling, depending on recieved argument <<<<<<<<<<<<<<<<<<<<<<<<----------------------------------------
+  /*toggleRow(canClose: boolean) {
     let status = !this.expanded$.getValue();
-    if (!canClose && !status) {
+     if (!canClose && !status) {
       return
     }
     this.expanded$.next(status);
+  }*/
+
+  // reverses the folder state from true to false or viceversa
+  toggleRow() {
+    let status = !this.expanded$.getValue();
+    this.expanded$.next(status);
   }
 
+
+  // this is a folder toggle event, but it is applied only on arrow icon
+  // why applied 2 different click events? one on arrow icon and another on folder name?
+  // when we can apply single click event on a single parent container which contains both of these elements?
+  /*
   toggleExpandFromArrow(event: MouseEvent) {
+    //console.clicked folder
+    console.log(this.folder);
+    console.log(this.expanded$.getValue());
     this.toggleRow(true);
     event.stopPropagation();
-  }
+  }*/
 
   // Changes the current folder and closes every active expandable
   toggleExpand() {
-    this.toggleRow(false); // Don't close when clicking into folder icon or text
+    // prevents folder closing when folder name or icon is clicked, careful, bad practice !!!
+    // this.toggleRow(false); 
+
+    // console currently clicked folder and its state(opened/closed)
+    // behaves as desired
+    console.log(this.folder);
+    console.log(this.expanded$.getValue());
+
+    // modify existing folder state, or add new instance of folder with its state
+    this.folderState[this.folder.name] = {
+      open: this.expanded$.getValue()
+    };
+
+    // refresh localstorage, so the next time this component view is rendered, it behaves correctly
+    localStorage.setItem('co_folderState', JSON.stringify(this.folderState));
+
+    // toggle folder (open/close)
+    this.toggleRow();
+
     if (this.folder.folder_id == 0) {
       this._store.dispatch(new Features.ReturnToFolderRoute(0));
     } else {
