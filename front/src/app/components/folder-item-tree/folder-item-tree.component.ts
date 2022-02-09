7cfca8f6d2068893b601ac84a6c77de9ee30d41e
toni <amvaratoni@gmail.com>
2022-02-09 16:26:31 +0000

    Fixed folder path persistance when accedding browser bookmarks

--- front/src/app/components/folder-item-tree/folder-item-tree.component.ts
+++ front/src/app/components/folder-item-tree/folder-item-tree.component.ts
@@ -5,7 +5,8 @@
  *
  * @author: dph000
  */
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Dispatch } from '@ngxs-labs/dispatch-decorator';
 import { Select, Store } from '@ngxs/store';
 import { CustomSelectors } from '@others/custom-selectors';
@@ -25,8 +26,7 @@ export class FolderItemTreeComponent implements OnInit {
   // stores state for each folder in hierarchy
   folderState = {};
 
-
-  constructor(private _store: Store) {
+  constructor(private _store: Store, private location: Location) {
     // get folder hierarchy state from localstorage, in case it is users first time entering, default department´s state will be set to false(closed)
     // if localstorage is empty, then set default values
     this.folderState =
@@ -49,7 +49,6 @@ export class FolderItemTreeComponent implements OnInit {
 
   // NgOnInit
   ngOnInit() {
-    // small change as per #3358,  check the modification reasoning in redmine
     if (this.folder.folder_id === 0) {
       this.expanded$ = new BehaviorSubject<boolean>(true);
     } else {
@@ -60,7 +59,6 @@ export class FolderItemTreeComponent implements OnInit {
       this.expanded$.next(true);
     }
   }
-
 
   /**
    * Dispatch functions
@@ -103,20 +101,6 @@ export class FolderItemTreeComponent implements OnInit {
    * General functions
    */
 
-  /**
-   * Function to toggle expanded state of current folder
-   * @param canClose If the current folder should close
-   */
-
-  // strange behavior, ask why limit toggling, depending on recieved argument <<<<<<<<<<<<<<<<<<<<<<<<----------------------------------------
-  /*toggleRow(canClose: boolean) {
-    let status = !this.expanded$.getValue();
-     if (!canClose && !status) {
-      return
-    }
-    this.expanded$.next(status);
-  }*/
-
   // reverses the folder state from true to false or viceversa
   toggleRow() {
     let status = !this.expanded$.getValue();
@@ -124,32 +108,25 @@ export class FolderItemTreeComponent implements OnInit {
   }
 
 
-  // this is a folder toggle event, but it is applied only on arrow icon
-  // why applied 2 different click events? one on arrow icon and another on folder name?
-  // when we can apply single click event on a single parent container which contains both of these elements?
-  /*
-  toggleExpandFromArrow(event: MouseEvent) {
-    //console.clicked folder
-    console.log(this.folder);
-    console.log(this.expanded$.getValue());
-    this.toggleRow(true);
-    event.stopPropagation();
-  }*/
-
   // Changes the current folder and closes every active expandable
   toggleExpand() {
-    // prevents folder closing when folder name or icon is clicked, careful, bad practice !!!
-    // this.toggleRow(false); 
-
-    // console currently clicked folder and its state(opened/closed)
-    // behaves as desired
-    // console.log(this.folder);
-    // console.log(this.expanded$.getValue());
-
     // modify existing folder state, or add new instance of folder with its state
     this.folderState[this.folder.name] = {
       open: this.expanded$.getValue()
     };
+
+    // #3414 -------------------------------------------------start
+    // folder url base
+    let folderUrl = "/new/";
+
+    // concat folder names to create path to clicked folder
+    this.parent.forEach(folder => {
+      folderUrl += `:${folder.folder_id}`;
+    })
+
+    // change url without redirection
+    this.location.go(folderUrl);
+    // #3414 ---------------------------------------------------end
 
     // refresh localstorage, so the next time this component view is rendered, it behaves correctly
     localStorage.setItem('co_folderState', JSON.stringify(this.folderState));
