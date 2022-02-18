957696d703e00e33a43c9574ebb068697fd95dba
Ralf Roeber <ralf.roeber@amvara.de>
2022-02-18 12:47:25 +0100

    Added api URL to request all featureResults for one featureId without featureRuns

--- backend/src/start.sh
+++ backend/src/start.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+# #######
+# This is the backend entrypoint, which starts the DJANGO Server
+# #######
+# 
+# As deault it will start gunicorn service compiling django, which is the fastest way and saves a lot of resources.
+# Only using gunicorn, you will be able to serve hundreds of users and executions on a small server setup.
+#
+# For development it is recommended to use the python django compiling on each change, so that you can change, see, tests and debug.
+# The Django server will be invoked using the parameter "-dev" on this script
+#
+# #######
+
+# set default to prod ... can be overwritten here or with commandline setting
+# prod executes gunicorn
+# dev executes ptyhon manage.py runserver
+ENVIRONMENT="prod"
+
+while [[ $# -gt 0 ]]
+do
+        key="$1"
+        case $key in
+                -d|--debug)
+                        set -x
+                        DEBUG=TRUE
+                        shift
+                        ;;
+                -dev|--development)
+                        echo "###################################################"
+                        echo "==> Setting development option requested from cli  "
+                        echo "###################################################"
+                        ENVIRONMENT="dev"
+                        shift
+                        ;;
+        esac
+done
+
+
 install_cron() {
     apt install -y cron
     touch /etc/cron.d/crontab
@@ -86,13 +123,30 @@ if [ ! -f "/code/.initiated" ]; then
     touch /code/.initiated
 fi
 
-# Start Django server
-# python manage.py runserver 0.0.0.0:8000
+#
+# in DEVMODE Start Django server
+#
+if [ "$ENVIRONMENT" = "dev" ]; then
+    echo "###################################################"
+    echo "# Running in DEV mode                             #"
+    echo "###################################################"
+    echo "Devmode was requested ... starting python manage.py runserver"
+    python manage.py runserver 0.0.0.0:8000
+fi
 
-# get processor cores
-CPUCORES=`getconf _NPROCESSORS_ONLN`
-# calculate workers
-GUNI_WORKERS=$((($CPUCORES*2+1)))
+#
+# In Production mode start gunicorn
+#
+if [ "$ENVIRONMENT" != "dev" ]; then
+    # get processor cores
+    CPUCORES=`getconf _NPROCESSORS_ONLN`
+    # calculate workers
+    GUNI_WORKERS=$((($CPUCORES*2+1)))
 
-# spin up gunicorn
-gunicorn cometa_pj.wsgi:application --workers=${WORKERS:-$GUNI_WORKERS} --threads=${THREADS:-2} --worker-class=gthread --bind 0.0.0.0:8000
+    # spin up gunicorn
+    echo "########################################################"
+    echo "# Running in production mode - starting gunicorn       #"
+    echo "# to enable dev mode, use parameter '-dev' on start.sh #"
+    echo "########################################################"
+    gunicorn cometa_pj.wsgi:application --workers=${WORKERS:-$GUNI_WORKERS} --threads=${THREADS:-2} --worker-class=gthread --bind 0.0.0.0:8000
+fi
