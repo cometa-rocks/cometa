7a0d621822627cdaa6af086f06f4b6203da1d394
Ralf Roeber <ralf.roeber@amvara.de>
2023-04-03 13:37:39 +0200

    #3928 - adding OIDCUser Information to variables JSON

--- backend/src/backend/serializers.py
+++ backend/src/backend/serializers.py
@@ -113,6 +113,11 @@ class BasicOIDCAccountSerializer(serializers.ModelSerializer):
         model = OIDCAccount
         fields = ['user_id', 'name', 'email']
 
+class BasicOIDCAccountSerializer_WithoutUserID(serializers.ModelSerializer):
+    class Meta:
+        model = OIDCAccount
+        fields = ['name', 'email']
+
 class OIDCAccountJsonSerializer(serializers.ModelSerializer):
     class Meta:
         model = OIDCAccount
@@ -493,6 +498,11 @@ class Folder_FeatureSerializer(serializers.ModelSerializer, FolderFeatureMixin):
 # Environment Variables model serializers #
 ###########################################
 class VariablesSerializer(serializers.ModelSerializer, VariablesMixin):
+
+    # retrieve OIDC account data without UserID
+    created_by = BasicOIDCAccountSerializer_WithoutUserID(many=False)
+    updated_by = BasicOIDCAccountSerializer_WithoutUserID(many=False)
+
     class Meta:
         model = Variable
         fields = '__all__'
