8490573622fecef00960bf84c0a4e3e46025a35d
toni <amvaratoni@gmail.com>
2023-04-05 14:14:42 +0000

    fixed bugs described in #3985

--- front/src/app/services/file-upload.service.ts
+++ front/src/app/services/file-upload.service.ts
@@ -82,7 +82,7 @@ export class FileUploadService {
     });
   }
 
-  // Opens informative snackbar when file user intends to upload could be uploaded because of duplication or potential virus
+  // Opens informative snackbar when file user intends to upload but file can't be uploaded because of duplication or potential virus
   private informAndRemoveFile(file: UploadedFile, department: Department) {
     let snack = this._snack.open(file.error.description, 'OK', { duration: 10000 });
 
@@ -99,8 +99,7 @@ export class FileUploadService {
 
   // removes recieved file from recieved department's files array and actualises the department state
   removeFile(file: UploadedFile, department: Department) {
-    const files = department.files.filter((f: UploadedFile) => f.name != file.name)
-
+    const files = department.files.filter((f: UploadedFile) => f.id != null)
     const payload = {
       files: [
         ...files
