673583818a5d211020ea9f96d838b6ab57332869
Arslan Sohail Bano <hello.arslan@amvara.de>
2023-04-28 16:18:34 +0200

    fix (logging): added logging for production, it should now show and save exceptions in console when DEBUG is set to false

--- backend/src/cometa_pj/settings.py
+++ backend/src/cometa_pj/settings.py
@@ -202,39 +202,67 @@ USERIDFLAG = "OPTIONAL"
 # LOGGER CONFIGURATION
 # From: https://stackoverflow.com/questions/52004910/django-and-docker-outputting-information-to-console
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "[{asctime}] {levelname} {name}:{funcName} [{threadName}({thread}):{process}] - {message}",
-            "style": "{",
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
         },
-        "simple": {
-            "format": "[{asctime}] {levelname} - {message}",
-            "style": "{",
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
         },
     },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-            "level": "DEBUG"
+    'formatters': {
+        'verbose': {
+            "format": "[%(asctime)s][%(levelname)s][%(process)d:%(processName)s][%(thread)d:%(threadName)s] - %(message)s"
         },
-        "errorFile": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "verbose",
-            "level": "ERROR",
-            "filename": "/opt/code/logs/error.log",
-            "maxBytes": 1000000, # 1MB
-            "backupCount": 4,
-            "encoding": "utf-8"
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[%(server_time)s] %(message)s',
         }
     },
-    "loggers": {
-        "*": {
-            "handlers": ["errorFile", "console"],
-            "level": "DEBUG",
-            "propagate": True
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        # Custom handler which we will use with logger 'django'.
+        # We want errors/warnings to be logged when DEBUG=False
+        'console_on_not_debug': {
+            'level': 'WARNING',
+            'filters': ['require_debug_false'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'error_file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': "logs/error.log",
+            'maxBytes': 1000000,
+            'backupCount': 7,
+            'formatter': 'verbose'
         }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins', 'console_on_not_debug', 'error_file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
     }
 }
