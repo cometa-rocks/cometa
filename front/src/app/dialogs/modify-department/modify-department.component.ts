fec9d726cc5bed153d47cd3e41a5cfdce3235069
toni <amvaratoni@gmail.com>
2023-05-05 14:37:06 +0200

    adapted layout styles, both set department timeout and modify all department timeouts are now working

--- front/src/app/dialogs/modify-department/modify-department.component.ts
+++ front/src/app/dialogs/modify-department/modify-department.component.ts
@@ -20,6 +20,7 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 export class ModifyDepartmentComponent {
 
   rForm: UntypedFormGroup;
+  timeoutForm: UntypedFormGroup;
   loading: boolean = false;
 
   expireDaysChecked$ = new BehaviorSubject<boolean>(false);
@@ -44,9 +45,11 @@ export class ModifyDepartmentComponent {
       'department_name': [this.department.department_name, Validators.required],
       'continue_on_failure': [{value: this.department.settings.continue_on_failure, disabled: this.account?.settings?.continue_on_failure}],
       'step_timeout': [this.department.settings?.step_timeout || 60, [Validators.required, Validators.compose([Validators.min(1), Validators.max(1000), Validators.maxLength(4)])]],
+      'result_expire_days': [expireDays]
+    });
+    this.timeoutForm = this.fb.group({
       'step_timeout_from': ['', Validators.compose([Validators.min(1), Validators.max(1000), Validators.maxLength(4)])],
       'step_timeout_to': ['', Validators.compose([Validators.min(1), Validators.max(1000), Validators.maxLength(4)])],
-      'result_expire_days': [expireDays]
     });
     this.expireDaysChecked$.next(!!this.department.settings.result_expire_days)
   }
@@ -62,19 +65,34 @@ export class ModifyDepartmentComponent {
   }
 
   applyGlobalTimeout(ev: Event) {
+    const options = this.timeoutForm.value;
     // prevents whole popup from closing
     ev.preventDefault();
 
     // disable button while http post is processed
     this.loading = true;
+    this._api.applyDepartmentStepsTimeout(this.department_id, options).subscribe({
+      next: (res) => {
+        let result = JSON.parse(res);
 
-    // todo:
-      // make post request to backend
-      // inform user with snackbar about the result
-      // enable button again
+        // if timeout modification XHR was successfull, show user how many steps and features were modified
+        if (result.success) {
+          result.total_steps_updated === 0 ?  this.snack.open("No steps with specified timeout", 'OK') :
+                                              this.snack.open(`features updated:${result.total_features_updated}, steps updated:${result.total_steps_updated}`, 'OK');
+        }
+        // enable button again
+        this.loading = false;
+        // reset input values
+        this.resetGlobalTimeoutInputs();
+      },
+      error: (err) => {
+        let error = JSON.parse(err.error);
 
-    // reset input global timeout input values
-    this.resetGlobalTimeoutInputs();
+        // show user the cause of error
+        this.snack.open(error.error, 'OK')
+        this.loading = false;
+      }
+    })
   }
 
   modifyDepartment(values) {
@@ -90,7 +108,7 @@ export class ModifyDepartmentComponent {
     this._api.modifyDepartment(this.department_id, payload).subscribe(res => {
       if (res.success) {
         this._store.dispatch( new Departments.UpdateDepartment(this.department_id, payload) );
-        this.dialogRef.close();
+        // this.dialogRef.close();
         this.snack.open('Department modified successfully!', 'OK');
       } else {
         this.snack.open('An error ocurred', 'OK');
@@ -100,7 +118,7 @@ export class ModifyDepartmentComponent {
 
   // resets the input value for global timeout modifiers
   resetGlobalTimeoutInputs() {
-    this.rForm.get('step_timeout_from').setValue('');
-    this.rForm.get('step_timeout_to').setValue('');
+    this.timeoutForm.get('step_timeout_from').setValue('');
+    this.timeoutForm.get('step_timeout_to').setValue('');
   }
 }
