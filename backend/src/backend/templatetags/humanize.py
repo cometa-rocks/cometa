602cf344f93260ef3b8140d786fed80e92021fff
Arslan <hello.arslan@amvara.de>
2023-07-27 11:21:02 +0200

    Feat/integration

--- backend/src/backend/templatetags/humanize.py
+++ backend/src/backend/templatetags/humanize.py
@@ -5,16 +5,7 @@ register = template.Library()
 
 @register.filter
 def Humanize(milliseconds):
-    milliseconds = int(milliseconds)
-    if milliseconds < 1000:
-        return "%dms" % milliseconds
-    seconds = int((milliseconds / 1000) % 60)
-    minutes = int((milliseconds / (1000 * 60)) % 60)
-    hours = int((milliseconds / (1000 * 60 * 60)) % 24)
-    hDisplay = (str(hours) + 'h' if hours > 0 else '') + (':' if minutes > 0 and hours > 0 else '')
-    mDisplay = (str(minutes) + 'm' if minutes > 0 else '') + (':' if seconds > 0 and minutes > 0 else '')
-    sDisplay = str(seconds) + 's' if seconds > 0 else ''
-    return hDisplay + mDisplay + sDisplay
+    return _humanize(milliseconds)
 
 @register.filter
 def NormalizeDownloadName(path):
@@ -27,3 +18,16 @@ def NormalizeDownloadName(path):
 @register.filter
 def FormatNumber(number):
     return '{:,}'.format(number)
+
+
+def _humanize(milliseconds):
+    milliseconds = int(milliseconds)
+    if milliseconds < 1000:
+        return "%dms" % milliseconds
+    seconds = int((milliseconds / 1000) % 60)
+    minutes = int((milliseconds / (1000 * 60)) % 60)
+    hours = int((milliseconds / (1000 * 60 * 60)) % 24)
+    hDisplay = (str(hours) + 'h' if hours > 0 else '') + (':' if minutes > 0 and hours > 0 else '')
+    mDisplay = (str(minutes) + 'm' if minutes > 0 else '') + (':' if seconds > 0 and minutes > 0 else '')
+    sDisplay = str(seconds) + 's' if seconds > 0 else ''
+    return hDisplay + mDisplay + sDisplay
