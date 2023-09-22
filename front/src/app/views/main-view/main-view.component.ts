85d7db0441eeca06eb92840112a93dd365880eeb
Arslan Sohail Bano <hello.arslan@amvara.de>
2023-09-22 18:04:12 +0200

    feat (results): update results table

--- front/src/app/views/main-view/main-view.component.ts
+++ front/src/app/views/main-view/main-view.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, ofActionDispatched, Select } from '@ngxs/store';
 import { combineLatest, Observable, fromEvent } from 'rxjs';
 import { CustomSelectors } from '@others/custom-selectors';
-import { NetworkPaginatedListComponent } from '@components/network-paginated-list/network-paginated-list.component';
-import { WebSockets } from '@store/actions/results.actions';
 import { map } from 'rxjs/operators';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { startWith } from 'rxjs/operators';
 import { Store } from '@ngxs/store';
 import { MainViewFieldsDesktop, MainViewFieldsMobile, MainViewFieldsTabletLandscape, MainViewFieldsTabletPortrait } from '@others/variables';
 import { MtxGridColumn } from '@ng-matero/extensions/grid';
 import { HttpClient } from '@angular/common/http';
 import { PageEvent } from '@angular/material/paginator';
+import { SharedActionsService } from '@services/shared-actions.service';
+import { WebSockets } from '@store/actions/results.actions';
+import { Configuration } from '@store/actions/config.actions';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { VideoComponent } from '@dialogs/video/video.component';
 
 @UntilDestroy()
 @Component({
@@ -21,26 +24,24 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./main-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainViewComponent implements OnInit, AfterViewInit {
-
-  isloaded: boolean = false;
-
-  @ViewChild(NetworkPaginatedListComponent, { static: false }) paginatedList: NetworkPaginatedListComponent;
+export class MainViewComponent implements OnInit {
 
   @Select(CustomSelectors.GetConfigProperty('internal.showArchived')) showArchived$: Observable<boolean>;
 
   columns: MtxGridColumn[] = [
-    {header: 'Status', field: 'status', sortable: true},
-    {header: 'Execution Date', field: 'result_date', sortable: true},
-    {header: 'Steps Total', field: 'total', sortable: true},
-    {header: 'Steps OK', field: 'ok', sortable: true},
-    {header: 'Steps NOK', field: 'fails', sortable: true},
-    {header: 'Steps Skipped', field: 'skipped', sortable: true},
-    {header: 'Duration', field: 'execution_time', sortable: true},
-    {header: 'Pixel Difference', field: 'pixel_diff', sortable: true},
+    {header: 'Status', field: 'status', sortable: true, class: 'aligned-center'},
+    {header: 'Execution Date', field: 'result_date', sortable: true, width: '230px', sortProp: { start: 'desc', id: 'result_date'}},
+    {header: 'Total', field: 'total', sortable: true, class: 'aligned-center'},
+    {header: 'OK', field: 'ok', sortable: true, class: 'aligned-center'},
+    {header: 'NOK', field: 'fails', sortable: true, class: 'aligned-center'},
+    {header: 'Skipped', field: 'skipped', class: 'aligned-center'},
+    {header: 'Browser', field: 'browser', class: 'aligned-center'},
+    {header: 'Browser Version', field: 'browser.browser_version', hide: true, sortable: true, class: 'aligned-center'},
+    {header: 'Duration', field: 'execution_time', sortable: true, class: "aligned-right"},
+    {header: 'Pixel Difference', field: 'pixel_diff', sortable: true, class: "aligned-right"},
     {
-      header: 'Operation',
-      field: 'operation',
+      header: 'Options',
+      field: 'options',
       width: '230px',
       pinned: 'right',
       right: '0px',
@@ -52,7 +53,8 @@ export class MainViewComponent implements OnInit, AfterViewInit {
           icon: 'videocam',
           tooltip: 'View results replay',
           color: 'primary',
-          click: () => alert('copy'),
+          disabled: (result: FeatureResult) => !result.video_url ? true : false,
+          click: (result: FeatureResult) => this.openVideo(result),
         },
         {
           type: 'icon',
@@ -60,15 +62,7 @@ export class MainViewComponent implements OnInit, AfterViewInit {
           icon: 'picture_as_pdf',
           tooltip: 'Download result PDF',
           color: 'primary',
-          click: () => alert('copy'),
-        },
-        {
-          type: 'icon',
-          text: 'delete',
-          icon: 'delete',
-          tooltip: 'Delete result',
-          color: 'warn',
-          click: () => alert('copy'),
+          click: (result: FeatureResult) => alert('copy'),
         },
         {
           type: 'icon',
@@ -76,7 +70,15 @@ export class MainViewComponent implements OnInit, AfterViewInit {
           icon: 'archive',
           tooltip: 'Archive result',
           color: 'accent',
-          click: () => alert('copy'),
+          click: (result: FeatureResult) => alert('copy'),
+        },
+        {
+          type: 'icon',
+          text: 'delete',
+          icon: 'delete',
+          tooltip: 'Delete result',
+          color: 'warn',
+          click: (result: FeatureResult) => alert('copy'),
         }
       ]
     }
@@ -101,14 +103,23 @@ export class MainViewComponent implements OnInit, AfterViewInit {
     private _actions: Actions,
     private _store: Store,
     private _router: Router,
-    private _http: HttpClient
+    public _sharedActions: SharedActionsService,
+    private _http: HttpClient,
+    private cdRef: ChangeDetectorRef,
+    private _dialog: MatDialog,
+    private _snack: MatSnackBar
   ) { }
 
-  featureRunsUrl$: Observable<string>;
   featureId$: Observable<number>;
 
   openContent(feature_result: FeatureResult) {
-    console.log(feature_result)
+    this._router.navigate([
+      this._route.snapshot.paramMap.get('app'),
+      this._route.snapshot.paramMap.get('environment'),
+      this._route.snapshot.paramMap.get('feature'),
+      'step',
+      feature_result.feature_result_id
+    ]);
   }
 
   getResults() {
@@ -120,101 +131,71 @@ export class MainViewComponent implements OnInit, AfterViewInit {
           archived: archived,
           ...this.params
         }
-      }).subscribe(
-        (res: any) => {
+      }).subscribe({
+        next: (res: any) => {
           this.results = res.results
           this.total = res.count
-          this.isLoading = false
-          console.log(this.results, this.total, this.isLoading)
         },
-        () => {
-          this.isLoading = false
+        error: (err) => {
+          console.error(err)
         },
-        () => {
+        complete: () => {
           this.isLoading = false
+          this.cdRef.detectChanges();
         }
-      )
+      })
     })
   }
 
-  getNextPage(e: PageEvent) {
+  updateData(e: PageEvent) {
     this.query.page = e.pageIndex
     this.query.size = e.pageSize
     this.getResults()
+
+    // create a localstorage session
+    localStorage.setItem('co_results_page_size', e.pageSize.toString())
+  }
+
+  /**
+   * Performs the overriding action through the Store
+   */
+  setResultStatus(results: FeatureResult, status: 'Success' | 'Failed' | '') {
+    this._sharedActions.setResultStatus(results, status).subscribe(_ => {
+      this.getResults();
+    })
+  }
+
+  openVideo(result: FeatureResult) {
+    this._sharedActions.loadingObservable(
+      this._sharedActions.checkVideo(result.video_url),
+      'Loading video'
+    ).subscribe({
+      next: _ => {
+        this._dialog.open(VideoComponent, {
+          backdropClass: 'video-player-backdrop',
+          panelClass: 'video-player-panel',
+          data: result
+        })
+      },
+      error: err => this._snack.open('An error ocurred', 'OK')
+    })
   }
 
   ngOnInit() {
     this.featureId$ = this._route.paramMap.pipe(
       map(params => +params.get('feature'))
     )
+    this.query.size = parseInt(localStorage.getItem('co_results_page_size')) || 10;
     this.getResults()
 
-    // Subscribe to URL params
-    this.featureRunsUrl$ = combineLatest([
-      // Get featureId parameter
-      this.featureId$,
-      // Get latest value from archived$
-      this.showArchived$
-    ]).pipe(
-      // Return endpointUrl for paginated list
-      map(([featureId, archived]) => `feature_results_by_featureid/?feature_id=${featureId}&archived=${archived}`)
-    )
     // Reload current page of runs whenever a feature run completes
     this._actions.pipe(
       untilDestroyed(this),
       ofActionDispatched(WebSockets.FeatureRunCompleted)
     ).subscribe(_ => {
-      if (this.paginatedList) this.paginatedList.reloadCurrentPage().subscribe()
+      this.getResults()
     });
 
-  }
-
-  ngAfterViewInit() {
-    // Change the run elements' visibility whenever the user changes the window size
-    combineLatest([
-      this._store.select(CustomSelectors.RetrieveResultHeaders(true)).pipe(
-        // Add some virtual headers
-        map(headers => ([
-          { id: 'bar', enable: true },
-          // @ts-ignore
-          ...headers,
-          { id: 'video', enable: true},
-          { id: 'options', enable: true }
-        ]))
-      ),
-      fromEvent(window, 'resize').pipe(
-        map((event: Event) => (event.target as Window).innerWidth),
-        startWith(window.innerWidth)
-      )
-    ]).pipe(
-      untilDestroyed(this)
-    ).subscribe(([headers, windowWidth]) => {
-      var showVariables = [];
-      if (windowWidth < 600) {
-        // Mobile
-        showVariables = MainViewFieldsMobile;
-      } else if (windowWidth < 900) {
-        // Tablet Portrait
-        showVariables = MainViewFieldsTabletPortrait;
-      } else if (windowWidth < 1200) {
-        // Tablet Landscape
-        showVariables = MainViewFieldsTabletLandscape;
-      } else {
-        // Desktop
-        showVariables = MainViewFieldsDesktop;
-      }
-      for (let i = 0; i < headers.length; i++) {
-        if (showVariables.includes(headers[i].id)) {
-          document.documentElement.style.setProperty(`--${headers[i].id}-display`, headers.find(header => header.id === headers[i].id).enable ? 'flex' : 'none');
-          document.documentElement.style.setProperty(`--${headers[i].id}-order`, (headers.findIndex(header => header.id === headers[i].id) + 1).toString());
-        } else {
-          document.documentElement.style.setProperty(`--${headers[i].id}-display`, 'none');
-          document.documentElement.style.setProperty(`--${headers[i].id}-order`, '0');
-        }
-      }
-    })
-
-    this.isloaded = true;
   }
 
   // return to v2 dashboard
