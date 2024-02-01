d72427922f864edb1d1c65f2295eed9fa07b8963
anand.kushwaha@teamteki.com <anand.kushwaha@teamteki.com>
2024-02-02 00:18:34 +0530

    API view implemented

--- backend/src/backend/ee/modules/security/serializers.py
+++ backend/src/backend/ee/modules/security/serializers.py
@@ -3,22 +3,24 @@
 # ###
 
 from rest_framework import serializers
-from .models import (
-    REST_API
-)
-import json
+from .models import ResponseHeaders
+from backend.views import GetUserDepartments
+
 
 ################################
-# Rest API model serializers #
+# Network Response Headers model serializers #
 ################################
-class RESTAPISerializer(serializers.ModelSerializer):
-    call = serializers.SerializerMethodField()
+class ResponseHeadersSerializer(serializers.ModelSerializer):
+    # Allow to send data as json from another model object
+
     class Meta:
-        model = REST_API
-        fields = '__all__'
-        depth = 0
-    
-    def get_call(self, instance):
-        if type(instance.call) == dict:
-            return instance.call
-        return json.loads(instance.call)
+        model = ResponseHeaders
+        # This fields also shows in the Django Rest Framework screen
+        fields = ['id', 'feature_result', 'department', 
+                  'vulnerable_headers_info', 'headers_count','created_on']
+
+    def validate(self, data):
+        departments = GetUserDepartments(self.context['request'])
+        if data.get('department') not in departments:
+            raise serializers.ValidationError("You do not belong to this department")
+        return data
