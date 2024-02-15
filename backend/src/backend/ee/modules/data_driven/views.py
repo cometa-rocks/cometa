918d43a0009b99805632b8738d786983b2ac13f0
anand.kushwaha@teamteki.com <anand.kushwaha@teamteki.com>
2024-02-15 22:36:51 +0530

    Rebase done

--- backend/src/backend/ee/modules/data_driven/views.py
+++ backend/src/backend/ee/modules/data_driven/views.py
@@ -6,7 +6,8 @@ from backend.models import (
     File,
     Feature,
     Feature_result,
-    FileData
+    FileData,
+    Feature_Task
 )
 from backend.serializers import (
     FeatureResultSerializer,
@@ -20,7 +21,9 @@ from backend.views import (
 )
 from concurrent.futures import (
     ThreadPoolExecutor,
-    as_completed
+    as_completed,
+    TimeoutError,
+    ThreadPoolExecutor
 )
 from backend.utility.decorators import (
     require_permissions,
@@ -35,6 +38,8 @@ from backend.utility.uploadFile import getFileContent
 from .models import DataDriven_Runs
 from .serializers import DataDrivenRunsSerializer
 import time, json
+from django.views.decorators.http import require_http_methods
+import requests
 
 
 class DataDrivenResultsViewset(viewsets.ModelViewSet):
@@ -189,13 +194,17 @@ def dataDrivenExecution(request, row, ddr: DataDriven_Runs):
             'scope': 'data-driven'
         } for k,v in data.items()
     ]
-
+    ddr.refresh_from_db()
+    logger.debug(f"Checking ddr status {ddr}")
+    if not ddr.running:
+        return
     result = runFeature(request, feature.feature_id, additional_variables=additional_variables)
     
     if not result['success']:
         raise Exception("Feature execution failed: %s" % result['error'])
     
     feature_result_ids = result['feature_result_ids']
+    logger.debug(f"Executions started with data driven run id {ddr.run_id} ")
     ddr.feature_results.add(*feature_result_ids)
 
     MAX_EXECUTION_TIMEOUT=7500
@@ -205,7 +214,8 @@ def dataDrivenExecution(request, row, ddr: DataDriven_Runs):
         while True:
             try:
                 fr = Feature_result.objects.get(pk=feature_result_id)
-                if fr.running and (time.time() - start) < MAX_EXECUTION_TIMEOUT:
+                ddr.refresh_from_db()  # Refresh data from DB if it DDT was stopped in between  
+                if fr.running and (time.time() - start) < MAX_EXECUTION_TIMEOUT and ddr.running:
                     logger.debug(f"Feature Result {fr.feature_result_id} is still running, will wait for it.")
                     time.sleep(10)
                 else:
@@ -226,20 +236,42 @@ def dataDrivenExecution(request, row, ddr: DataDriven_Runs):
 def startDataDrivenRun(request, rows: list[FileData], ddr: DataDriven_Runs):
     user = request.session['user']
     logger.info(f"Starting Data Driven Test {user['name']}")
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        futures = []
-        for row in rows:
-            logger.info(row)
-            futures.append(executor.submit(dataDrivenExecution, request, row, ddr))
-        
-        for future in as_completed(futures):
-            try:
-                logger.info(future.result())
-            except Exception as err:
-                logger.exception(err)
     
+    try:
+        # This executes one row data at a time with selected browsers 
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            futures = []
+            
+            for row in rows:
+                futures.append(executor.submit(dataDrivenExecution, request, row, ddr))
+            
+            for future in as_completed(futures):
+                try:
+                    ddr.refresh_from_db()
+                    logger.debug(f"Checking on ddr status {ddr}")
+                    if not ddr.running:
+                        executor.shutdown(wait=False,cancel_futures=True)
+                        break
+
+                except (TimeoutError, ThreadPoolExecutor) as exception:
+                    logger.exception(exception)
+
+                try:
+                    logger.info(future.result())
+                except Exception as err:
+                    logger.exception(err)
+
+    except Exception as exception:
+        logger.exception(exception)
+    
+    # Update when test completed (If stopped or completed in both cases) 
+    ddr.running = False
     ddr.status = 'Failed' if ddr.fails > 0 else 'Passed'
     ddr.save()
+    # Update to cometa Socket
+    response = requests.post(f'http://cometa_socket:3001/dataDrivenStatus/{ddr.run_id}',{"running":False})
+    if response.status_code != 200 :
+        raise Exception("Not able to connect cometa_socket")
 
 @csrf_exempt
 @require_subscription()
@@ -270,9 +302,11 @@ def runDataDriven(request, *args, **kwargs):
             "success": False,
             "error": "No rows found for selected data driven file."
         }, status=400)
+    
     try:
-        ddr = DataDriven_Runs(file_id=file_id, status="Running")
+        ddr = DataDriven_Runs(file_id=file_id, running=True, status="Running")
         ddr.save()
+        requests.post(f'http://cometa_socket:3001/dataDrivenStatus/{ddr.run_id}',{"running":True})
         # Spawn thread to launch feature run
         t = Thread(target=startDataDrivenRun, args=(request, file_data, ddr))
         t.start()
@@ -280,3 +314,52 @@ def runDataDriven(request, *args, **kwargs):
         return JsonResponse({ 'success': True, 'run_id': ddr.pk })
     except Exception as e:
         return JsonResponse({ 'success': False, 'error': str(e) })
+    
+@csrf_exempt
+@require_subscription()
+@require_permissions("run_feature")
+@require_http_methods(["POST"])
+def stop_data_driven_test(request, *args, **kwargs):  
+    run_id = kwargs.get('run_id', None)
+    if not run_id:
+        return JsonResponse({ 'success': False, 'error': 'Missing \'run_id\'' }, status=400)
+    
+    try:
+        data_driven_run = DataDriven_Runs.objects.get(run_id=run_id)
+    except DataDriven_Runs.DoesNotExist:
+        return JsonResponse({
+            "success": False,
+            "error": f"data driven run not found with id : {run_id}"
+        }, status=404)
+    # Get all feature results ID
+    feature_results = data_driven_run.feature_results.all()
+
+    # Get all task ID which are running feature with different browsers and test data
+    tasks = Feature_Task.objects.filter(feature_result_id__in=feature_results)
+
+    try:    
+        logger.debug(f"Found total {len(tasks)} tasks related to data driven run id : {run_id}")
+        for task in tasks:
+            task.feature_result_id.running = False
+            task.feature_result_id.save()
+            response = requests.get('http://behave:8001/kill_task/' + str(task.pid) + "/")
+            logger.debug(f"Killing Task with id {task.task_id}")
+            task.delete()
+        
+        
+        if len(tasks) > 0:
+            # Force state of stopped for current feature in WebSocket Server
+            response = requests.post(f'http://cometa_socket:3001/dataDrivenStatus/{run_id}',{"running":False})
+            if response.status_code != 200 :
+                raise Exception("Not able to connect cometa_socket")
+
+        # Force state of stopped for current feature in WebSocket Server
+        data_driven_run.running = False
+        data_driven_run.status = "Terminated"
+        data_driven_run.save()
+
+        return JsonResponse({"success": True, "tasks": len(tasks),"run_id":run_id}, status=200)
+
+    except Exception as e:
+        logger.exception(e)
+        return JsonResponse({"success": False, "tasks": len(tasks)}, status=500)
