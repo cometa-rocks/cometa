9703b01d4b203aef4ec83ff1b93a280a17a6ee20
REDOUAN NATI <hello.redouan@amvara.de>
2024-06-03 10:57:17 +0200

    Changed part of hotkeys shortcuts, add tables on help (hotkeys)

--- front/src/app/components/help/help.component.ts
+++ front/src/app/components/help/help.component.ts
@@ -6,27 +6,72 @@ import { Observable } from 'rxjs';
 import { SortByPipe } from '@pipes/sort-by.pipe';
 import { NgFor, AsyncPipe } from '@angular/common';
 
+// Mtx table hotkeys
+import { MtxGridColumn } from '@ng-matero/extensions/grid';
+import { MtxGridModule } from '@ng-matero/extensions/grid';
+
+
+interface Shortcut {
+  position: number;
+  key: string;
+  description: string;
+}
+
 @Component({
   selector: 'cometa-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgFor, SortByPipe, AsyncPipe],
+  imports: [NgFor, SortByPipe, AsyncPipe, MtxGridModule],
 })
 export class HelpComponent {
   @Select(ActionsState) actions$: Observable<Action[]>;
   @Select(ConfigState) config$: Observable<Config>;
 
-  hotkeys = [
-    { hotkey: 'ESC', text: 'Quit' },
-    { hotkey: 'Space', text: 'Run Feature' },
-    { hotkey: 'E', text: 'Edit Feature' },
-    { hotkey: 'L', text: 'Show Log Output' },
-    { hotkey: 'N', text: 'Enable/Disable notifications' },
-    { hotkey: 'S', text: 'Edit Schedule' },
-    { hotkey: 'Shift + Alt + F', text: 'Open search' },
-    { hotkey: 'Shift + Alt + H', text: 'Return to home' },
-    { hotkey: 'Shift + Alt + X', text: 'Remove all the filters' },
+  ShortMainViewFeature: Shortcut[] = [
+    { position: 1, key: 'ESC', description: 'Quit the window' },
+    { position: 2, key: 'SPACE', description: 'Run Feature' },
+    { position: 3, key: 'E', description: 'Edit Feature' },
+    { position: 4, key: 'L', description: 'Show Log Output' },
+    { position: 5, key: 'N', description: 'Enable/Disable notifications' },
+    { position: 6, key: 'S', description: 'Edit Schedule' },
   ];
+
+  ShortMainPage: Shortcut[] = [
+    { position: 1, key: 'A', description: '[+ Add] button: Add from side nav' },
+    { position: 2, key: 'B', description: '[B] button: Create folder' },
+    { position: 1, key: 'F', description: '[F] button: Create feature' },
+    { position: 7, key: 'Shift + Alt + F', description: 'Open search'},
+    { position: 8, key: 'Shift + Alt + H', description: 'Return to home'},
+    { position: 9, key: 'Shift + Alt + X', description: 'Remove all the filters'},
+  ];
+
+  ShortDataDriven: Shortcut[] = [
+    { position: 1, key: 'T', description: '[Data Driven Test] button: Execution of tests based on the variables proivided by the excel sheet' },
+  ];
+
+  ShortCreateEditFeature: Shortcut[] = [
+    { position: 1, key: 'D', description: 'Checkbox: Depends on other feature' },
+    { position: 2, key: 'F', description: 'Checkbox: Continue on failure' },
+    { position: 3, key: 'H', description: 'Checkbox: Ask for help' },
+    { position: 2, key: 'M', description: 'Checkbox: Send mail on finish' },
+    { position: 3, key: 'R', description: 'Checkbox: Record video' },
+    { position: 5, key: 'V', description: 'Button: Edit Variables' },
+  ];
+
+  columns: MtxGridColumn[] = [
+    { header: 'Shortcut', field: 'key' },
+    { header: 'Description', field: 'description' },
+  ];
+
+  listMainViewFeature = this.ShortMainViewFeature;
+  listMainPage = this.ShortMainPage;
+  listDataDriven = this.ShortDataDriven;
+  listCreateEditFeature = this.ShortCreateEditFeature;
+
+  trackByName(index: number, item: any) {
+    return item.name;
+  }
+
 }
