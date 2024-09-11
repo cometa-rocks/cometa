75b6fd990908e804791d19d74d3fa1998c16ce4f
anandkushwaha064 <hello.anand@amvara.de>
2024-09-11 12:19:10 +0200

    Correcte cometa_ai container start.sh and docker-compose file

--- cometa_ai/start.sh
+++ cometa_ai/start.sh
@@ -1,11 +1,14 @@
 # /bin/bash
 
 # To pull models of ollama, ollama server has to be running in an terminal
-nohup ollama serve &
+
+# export OLLAMA_HOST=0.0.0.0:11434
+
+ollama serve 
 
 # Pull the models 7b by default
-ollama pull llava
-ollama pull llama3.1
+# ollama pull llava
+# ollama pull llama3.1
 # ollama pull llava:13b
 
 # https://ollama.com/library/llava:13b
