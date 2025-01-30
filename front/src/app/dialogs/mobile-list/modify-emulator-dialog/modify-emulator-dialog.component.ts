0f4e23d5a4a246dd68e51e6ecb0eebaeb3f88579
anandkushwaha064 <hello.anand@amvara.de>
2025-01-30 17:14:52 +0100

    #5606 Changed logic of buttons restart and pause, correct buttons on more vert, created dialog of emulator

--- front/src/app/dialogs/mobile-list/modify-emulator-dialog/modify-emulator-dialog.component.ts
+++ front/src/app/dialogs/mobile-list/modify-emulator-dialog/modify-emulator-dialog.component.ts
@@ -17,26 +17,15 @@ import {
 } from '@angular/material/legacy-dialog';
 import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { BrowserFavouritedPipe } from '@pipes/browser-favourited.pipe';
-import { PlatformSortPipe } from '@pipes/platform-sort.pipe';
 import { BehaviorSubject, map, Observable, Subject, takeUntil } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateModule } from '@ngx-translate/core';
-import { SortByPipe } from '@pipes/sort-by.pipe';
-import { MobileIconPipe } from '@pipes/mobile-icon.pipe';
-import { BrowserComboTextPipe } from '../../pipes/browser-combo-text.pipe';
-import { VersionSortPipe } from '@pipes/version-sort.pipe';
-import { TranslateNamePipe } from '@pipes/translate-name.pipe';
-import { CheckBrowserExistsPipe } from '@pipes/check-browser-exists.pipe';
-import { CheckSelectedBrowserPipe } from '@pipes/check-selected-browser.pipe';
 import { MatLegacyProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { LetDirective } from '../../directives/ng-let.directive';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyButtonModule } from '@angular/material/legacy-button';
 import { MatLegacyCheckboxModule } from '@angular/material/legacy-checkbox';
 import { ContextMenuModule } from '@perfectmemory/ngx-contextmenu';
 import { MatLegacyTooltipModule } from '@angular/material/legacy-tooltip';
-import { StopPropagationDirective } from '../../directives/stop-propagation.directive';
 import { MatLegacyInputModule } from '@angular/material/legacy-input';
 import { MatLegacyOptionModule } from '@angular/material/legacy-core';
 import { MatLegacySelectModule } from '@angular/material/legacy-select';
@@ -49,21 +38,15 @@ import {
   TitleCasePipe,
   KeyValuePipe,
 } from '@angular/common';
-import { ApiService } from '@services/api.service';
 import { ViewSelectSnapshot } from '@ngxs-labs/select-snapshot';
-import { UserState } from '@store/user.state';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDividerModule } from '@angular/material/divider';
-import { DraggableWindowModule } from '@modules/draggable-window.module'
-import { AreYouSureData, AreYouSureDialog } from '@dialogs/are-you-sure/are-you-sure.component';
 import { Store } from '@ngxs/store';
-import { CustomSelectors } from '@others/custom-selectors';
-import { LogService } from '@services/log.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { timeStamp } from 'console';
-import { Key } from 'readline';
-import { ModifyEmulatorDialogComponent } from './components/modify-emulator-dialog/modify-emulator-dialog.component';
+import { DraggableWindowModule } from '@modules/draggable-window.module'
+import { MatExpansionModule } from '@angular/material/expansion';
+
 
 /**
  * MobileListComponent
@@ -74,9 +57,9 @@ import { ModifyEmulatorDialogComponent } from './components/modify-emulator-dial
  */
 @UntilDestroy()
 @Component({
-  selector: 'mobile-list',
-  templateUrl: './mobile-list.component.html',
-  styleUrls: ['./mobile-list.component.scss'],
+  selector: 'modify-emulator-dialog',
+  templateUrl: './modify-emulator-dialog.component.html',
+  styleUrls: ['./modify-emulator-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   // providers: [BrowserFavouritedPipe, PlatformSortPipe],
   standalone: true,
@@ -88,49 +71,36 @@ import { ModifyEmulatorDialogComponent } from './components/modify-emulator-dial
     NgFor,
     MatLegacyOptionModule,
     MatLegacyInputModule,
-    StopPropagationDirective,
     NgClass,
     MatLegacyTooltipModule,
     ContextMenuModule,
     MatLegacyCheckboxModule,
     MatLegacyButtonModule,
     MatIconModule,
-    LetDirective,
     MatLegacyProgressSpinnerModule,
     AsyncPipe,
     TitleCasePipe,
     KeyValuePipe,
-    PlatformSortPipe,
-    MobileIconPipe,
-    CheckSelectedBrowserPipe,
-    CheckBrowserExistsPipe,
-    TranslateNamePipe,
-    VersionSortPipe,
-    BrowserFavouritedPipe,
-    BrowserComboTextPipe,
-    SortByPipe,
     TranslateModule,
     MatLegacyDialogModule,
     MatSlideToggleModule,
     MatDividerModule,
-    DraggableWindowModule,
     FormsModule,
     MatMenuModule,
-    MatButtonToggleModule
-  ],
+    MatButtonToggleModule,
+    DraggableWindowModule,
+    MatExpansionModule
+],
 })
 export class ModifyEmulatorDialogComponent {
   constructor(
     private _dialog: MatDialog,
-    private _api: ApiService,
     private _cdr: ChangeDetectorRef,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private snack: MatSnackBar,
     private _store: Store,
-    private logger: LogService
   ) {
   }
-  @ViewSelectSnapshot(UserState) user!: UserInfo;
 
   // Declare the variable where the API result will be assigned
   mobiles: IMobile[] = [];
@@ -159,7 +129,7 @@ export class ModifyEmulatorDialogComponent {
   isLoading = true;
 
   ngOnInit(): void {
-
+    console.log("Data: ", this.data)
   }
 
 
