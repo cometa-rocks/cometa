a9b5a20331ba7e6a5874fe6a6a0a9cd68e01dbb3
anandkushwaha064 <hello.anand@amvara.de>
2025-02-06 01:31:35 +0530

    #5618 Releasing database testing feature

--- backend/src/backend/ee/modules/mobile/urls.py
+++ backend/src/backend/ee/modules/mobile/urls.py
@@ -2,11 +2,25 @@
 # Sponsored by Mercedes-Benz AG, Stuttgart
 # ###
 
-from .views import MobileViewSet
+from .views import MobileViewSet, parseCometaMobiles
 
+from django.conf.urls import url
 
 # Add new modules url here
 # This keeps url.py file short and clean
 def register_mobile_routers(router):
     router.register(r"mobile", MobileViewSet)
     return router
+
+
+
+
+# Add new modules url here
+# This keeps url.py file short and clean
+def register_mobile_urlpatterns(urlpatterns) :
+
+    urlpatterns = urlpatterns + [
+        url(r'^parse_mobiles/', parseCometaMobiles),
+    ]
+    return urlpatterns
+    
