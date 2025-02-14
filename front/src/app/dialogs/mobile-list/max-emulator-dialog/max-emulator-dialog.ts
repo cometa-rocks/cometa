2e7d88a039dba830731f162946ac356898e14918
anandkushwaha064 <hello.redouan@amvara.de>
2025-02-14 14:45:37 +0100

    #5606 Add fialog alert max emulator running, to do filter the running containers in slector

--- front/src/app/dialogs/mobile-list/max-emulator-dialog/max-emulator-dialog.ts
+++ front/src/app/dialogs/mobile-list/max-emulator-dialog/max-emulator-dialog.ts
@@ -1,40 +1,59 @@
-import { Component, Inject } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import {
   MatLegacyDialogRef as MatDialogRef,
+  MatLegacyDialog as MatDialog,
+  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
   MatLegacyDialogModule,
 } from '@angular/material/legacy-dialog';
-import { MatLegacyButtonModule } from '@angular/material/legacy-button';
 import { NgIf } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'max-emulator-dialog',
   templateUrl: 'max-emulator-dialog.html',
   styleUrls: ['max-emulator-dialog.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatLegacyDialogModule, NgIf, MatLegacyButtonModule, MatFormFieldModule,
+  imports: [
+    MatLegacyDialogModule,
+    NgIf,
+    MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    CommonModule
   ],
+  providers: [
+    { provide: MatDialogRef, useValue: {} }
+  ]
 })
 export class MaxEmulatorDialogComponent {
-  runningEmulators: { id: number; name: string }[];
-  departmentName: string;
+  departments: any;
+  emulatorsByDepartment: { [key: string]: { id: number; name: string }[] };
 
   constructor(
+    private _dialog: MatDialog,
     private dialogRef: MatDialogRef<MaxEmulatorDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) {
-    this.runningEmulators = data.runningEmulators;
-    this.departmentName = data.departmentName;
+
+  }
+
+  ngOnInit() {
+    // Asignamos los datos recibidos a las propiedades
+    this.departments = this.data.departments;
+    this.emulatorsByDepartment = this.data.emulatorsByDepartment;
+
+    console.log('Departments:', this.departments);
+    console.log('Emulators by department:', this.emulatorsByDepartment);
   }
 
   closeDialog() {
-    this.dialogRef.close();
+    this._dialog.closeAll();
   }
 }
