bcff8d85e15a2acd2e74efd0ef113af3d614bacb
wiatakillo <wistakito@gmail.com>
2025-03-20 04:08:26 -0700

    Implement AI Action browser-use

--- cometa_ai/Run.py
+++ cometa_ai/Run.py
@@ -3,6 +3,7 @@ from rq import Worker, Queue, Connection
 from src.connections.redis_connection import (
     connect_redis,
     REDIS_IMAGE_ANALYSYS_QUEUE_NAME,
+    REDIS_BROWSER_USE_QUEUE_NAME,
     REDIS_NUMBER_OF_WORKERS,
 )
 from src.utility.common import get_logger
@@ -15,7 +16,8 @@ def start_worker():
     REDIS_CONNECTION = connect_redis()
     with Connection(REDIS_CONNECTION):
         queue = Queue(REDIS_IMAGE_ANALYSYS_QUEUE_NAME, connection=REDIS_CONNECTION)
-        worker = Worker([queue])
+        queue_browser_use = Queue(REDIS_BROWSER_USE_QUEUE_NAME, connection=REDIS_CONNECTION)
+        worker = Worker([queue, queue_browser_use])
         worker.work()
 
 
