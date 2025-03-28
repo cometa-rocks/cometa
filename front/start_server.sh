5de33ad7044f66d9d83ebe9655f35d1c24d43522
anandkushwaha064 <hello.anand@amvara.de>
2025-03-28 15:57:18 +0100

    Updated front start_server.sh to fix favicon icon

--- front/start_server.sh
+++ front/start_server.sh
@@ -19,6 +19,37 @@ cat <<EOF > "/usr/local/apache2/htdocs/infra/lbtest1.html"
 </html>
 EOF
 
+
+function update_favicon_icon() {
+  # Define the mapping of DEPLOYMENT_TYPE to branch names
+  declare -A BRANCH_MAP=( ["production"]="master" ["stage"]="stage" )
+
+  # Get the branch name from the DEPLOYMENT_TYPE variable (default to "master")
+  BRANCH_NAME=${BRANCH_MAP[${DEPLOYMENT_TYPE}]:-master}
+
+  echo "DEPLOYMENT_TYPE: '${DEPLOYMENT_TYPE:-unset}'"
+  echo "Resolved branch name: '${BRANCH_NAME}'"
+
+  # Define files where replacement should happen
+  export COMETA_REPLACE_FAVICON_IN="/usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/manifest.json /usr/local/apache2/htdocs/welcome.html"
+
+  # Perform replacement in each file inside the Docker container
+  for FILE in ${COMETA_REPLACE_FAVICON_IN}; do
+    sed -i "s/@@BRANCH@@/${BRANCH_NAME}/g" "$FILE"
+    echo "Successfully updated $FILE"
+  done
+  
+  echo "Favicon update process completed."
+
+}
+
+function update_apache_conf() {
+  echo "Updating Apache httpd.conf and paths.conf"
+  cp /code/front/apache2/conf/httpd.conf /usr/local/apache2/conf/httpd.conf && echo "httpd.conf updated." || echo "Failed to update httpd.conf!" >&2
+  cp /code/front/apache2/conf/paths.conf /usr/local/apache2/conf/paths.conf && echo "paths.conf updated." || echo "Failed to update paths.conf!" >&2
+}
+
+
 # #########
 # This function checks if self signed SSL certificate exists
 # and it is not expired if so or missing it will create one
@@ -69,6 +100,11 @@ function check_ssl_certificate() {
         echo -e "\e[32mOK\e[0m"
 }
 
+# Update favicon icon
+update_favicon_icon
+
+# Update paths and httpd.conf
+update_apache_conf
 
 # check_ssl_certificate
 
--- scripts/fix_favicon.sh
+++ scripts/fix_favicon.sh
@@ -1,3 +1,21 @@
-export COMETA_REPLACE_FAVICON_IN="/usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/manifest.json /usr/local/apache2/htdocs/welcome.html" 
-for FILE in ${COMETA_REPLACE_FAVICON_IN}; do docker exec cometa_front sed -i 's/@@BRANCH@@/'master'/g' $FILE; done
-docker restart cometa_front
+#/bin/bash
+
+# Define the mapping of DEPLOYMENT_TYPE to branch names
+declare -A BRANCH_MAP=( ["production"]="master" ["stage"]="stage" )
+
+# Get the branch name from the DEPLOYMENT_TYPE variable (default to "master")
+BRANCH_NAME=${BRANCH_MAP[${DEPLOYMENT_TYPE}]:-master}
+
+echo "DEPLOYMENT_TYPE: '${DEPLOYMENT_TYPE:-unset}'"
+echo "Resolved branch name: '${BRANCH_NAME}'"
+
+# Define files where replacement should happen
+export COMETA_REPLACE_FAVICON_IN="/usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/manifest.json /usr/local/apache2/htdocs/welcome.html"
+
+# Perform replacement in each file inside the Docker container
+for FILE in ${COMETA_REPLACE_FAVICON_IN}; do
+sed -i "s/@@BRANCH@@/${BRANCH_NAME}/g" "$FILE"
+echo "Successfully updated $FILE"
+done
+
+echo "Favicon update process completed."
