7995bd5cbe2329fb137f315496e88663bb12c2e6
anandkushwaha064 <hello.anand@amvara.de>
2025-04-02 02:26:04 +0530

    Version updates, Imroved variables reporting

--- backend/behave/ee/cometa_itself/steps/common_actions.py
+++ backend/behave/ee/cometa_itself/steps/common_actions.py
@@ -142,7 +142,7 @@ def read_excel_step(context, file_path, sheet_name, row_number, variable_name):
         json_data = json.dumps(row_data, ensure_ascii=False)
         logger.debug(json_data)
         # Store the JSON data in runtime variables
-        addTestRuntimeVariable(context, variable_name, json_data)
+        addTestRuntimeVariable(context, variable_name, json_data, save_to_step_report=True)
         
         logger.debug(f"Stored data from Excel row {row_number} into variable '{variable_name}': {row_data}")
 
@@ -217,7 +217,7 @@ def assert_imp(context, jq_pattern, variable_name, new_variable_name):
 
     try:
         parsed_value = jq.compile(jq_pattern).input(variable_value).text()
-        addTestRuntimeVariable(context, new_variable_name, parsed_value)
+        addTestRuntimeVariable(context, new_variable_name, parsed_value, save_to_step_report=True)
     except Exception as err:
         logger.error("Invalid JQ pattern", err)
         raise CustomError(err)
@@ -263,7 +263,7 @@ def generate_fake_data_store_in_variable(context, information, variable):
         if callable(method):
             send_step_details(context, f"Generating {information}")
             value = method()
-            addTestRuntimeVariable(context, variable, value)
+            addTestRuntimeVariable(context, variable, value, save_to_step_report=True)
         else:
             raise CustomError(f"'Information type : {information}' not available. Available types are {get_faker_public_methods()}")
     else:
