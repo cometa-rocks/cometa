d679a4100bbaf3f2492163022819fdc507d57847
Nico Clariana <hello.nico@amvara.de>
2025-04-15 17:00:50 +0200

    [#6072] Initial component creation

---
+++ front/src/app/components/api-testing/api-testing.component.ts
@@ -0,0 +1,26 @@
+/**
+ * l1-feature-trashbin-list.component.ts
+ *
+ * Contains the code to control the behaviour of the features list within the trash bin of the new landing
+ *
+ * @date 08-10-21
+ *
+ * @lastModification 08-10-21
+ *
+ * @author: dph000
+ */
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'cometa-api-testing',
+  templateUrl: './api-testing.component.html',
+  styleUrls: ['./api-testing.component.scss'],
+  standalone: true,
+  imports: [MatIconModule],
+})
+export class ApiTestingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
