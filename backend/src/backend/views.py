d5e0a9ababe34d0ef87813de51d0907d58aa8160
anandkushwaha064 <hello.anand@amvara.de>
2025-04-24 16:39:03 +0530

    #6125 Investigating on the increased execution time

--- backend/src/backend/views.py
+++ backend/src/backend/views.py
@@ -2176,17 +2176,17 @@ class StepResultViewSet(viewsets.ModelViewSet):
             data['files'] = json.loads(data['files'])
         
         logger.debug(f"Saving Step {data['step_name']}")
-        logger.debug("Checking for last step")
-        last_step = Step_result.objects.filter(feature_result_id = data['feature_result_id']).order_by('-step_result_id').first()
-        logger.debug("Last step result checked")
-        if last_step:
-            # This will execute if it is not the report of the execution
-            logger.debug("Found last step relative calculating time")
-            data['relative_execution_time'] = last_step.relative_execution_time + data['execution_time']
-        else:
-            # This will execute if it is a first step report of the execution
-            logger.debug("Relative time Initated")
-            data['relative_execution_time'] = data['execution_time']
+        # logger.debug("Checking for last step")
+        # last_step = Step_result.objects.filter(feature_result_id = data['feature_result_id']).order_by('-step_result_id').first()
+        # logger.debug("Last step result checked")
+        # if last_step:
+        #     # This will execute if it is not the report of the execution
+        #     logger.debug("Found last step relative calculating time")
+        #     data['relative_execution_time'] = last_step.relative_execution_time + data['execution_time']
+        # else:
+        #     # This will execute if it is a first step report of the execution
+        #     logger.debug("Relative time Initated")
+        #     data['relative_execution_time'] = data['execution_time']
         logger.debug("Starting to save data to Step_result")
         step_result = Step_result.objects.create(**data)
         logger.debug("Data saved to Step_result")
@@ -4111,3 +4111,4 @@ from backend.ee.modules.data_driven.views import (
     DataDrivenFileViewset,
     DataDrivenResultsViewset
 )
+
