b3945bb9be9d47049f796ed68bc62b0aa0b4f0d4
redouan <hello.redouan@amvara.de>
2025-05-12 17:19:07 +0200

    restore-confirmation-dialog

--- front/src/app/components/restore-confirmation-dialog/restore-confirmation-dialog.component.ts
+++ front/src/app/components/restore-confirmation-dialog/restore-confirmation-dialog.component.ts
@@ -1,13 +1,19 @@
-import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { Component, Inject, ChangeDetectionStrategy } from '@angular/core';
+import { ApiService } from '@services/api.service';
 import {
-  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
   MatLegacyDialogRef as MatDialogRef,
+  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
   MatLegacyDialogModule,
 } from '@angular/material/legacy-dialog';
-import { TranslateService } from '@ngx-translate/core';
+import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { BehaviorSubject, of } from 'rxjs';
+import { finalize, map, switchMap } from 'rxjs/operators';
+import { SharedActionsService } from '@services/shared-actions.service';
+import { Features } from '@store/actions/features.actions';
+import { AsyncPipe } from '@angular/common';
 import { MatLegacyButtonModule } from '@angular/material/legacy-button';
+import { LetDirective } from '../../directives/ng-let.directive';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cometa-restore-confirmation-dialog',
@@ -15,17 +21,17 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./restore-confirmation-dialog.component.scss'],
   standalone: true,
   imports: [
-    CommonModule,
-    MatLegacyButtonModule,
     MatLegacyDialogModule,
+    LetDirective,
+    MatLegacyButtonModule,
+    AsyncPipe,
     MatIconModule
-  ]
+  ],
 })
 export class RestoreConfirmationDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<RestoreConfirmationDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _translate: TranslateService
   ) {}
 
   onConfirm(): void {
