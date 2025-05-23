60810ba3212e3115f5954421247b61819ed383bc
wiatakillo <wistakito@gmail.com>
2025-05-23 13:46:33 -0700

    Re-Added timezone support when scheduling a feature

--- front/src/app/dialogs/edit-feature/edit-feature.component.ts
+++ front/src/app/dialogs/edit-feature/edit-feature.component.ts
@@ -30,6 +30,7 @@ import {
   Validators,
   UntypedFormBuilder,
   ReactiveFormsModule,
+  FormsModule,
 } from '@angular/forms';
 import {
   MatLegacyCheckboxChange as MatCheckboxChange,
@@ -114,6 +115,7 @@ import { Departments } from '@store/actions/departments.actions';
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     NgIf,
     MatLegacyDialogModule,
     MatExpansionModule,
@@ -193,7 +195,100 @@ export class EditFeature implements OnInit, OnDestroy {
     msg: '',
   };
   // get user timezone
-  timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  // default to user's browser timezone
+  timezone = 'browser-timezone';
+
+  // Comprehensive list of timezones (based on tz database 2025b - canonical zones only)
+  allTimezones = [
+    'UTC', 'Etc/UTC', 'Etc/GMT',
+    // Americas
+    'America/Adak', 'America/Anchorage', 'America/Anguilla', 'America/Antigua', 'America/Araguaina',
+    'America/Argentina/Buenos_Aires', 'America/Argentina/Catamarca', 'America/Argentina/Cordoba', 
+    'America/Argentina/Jujuy', 'America/Argentina/La_Rioja', 'America/Argentina/Mendoza', 
+    'America/Argentina/Rio_Gallegos', 'America/Argentina/Salta', 'America/Argentina/San_Juan', 
+    'America/Argentina/San_Luis', 'America/Argentina/Tucuman', 'America/Argentina/Ushuaia',
+    'America/Aruba', 'America/Asuncion', 'America/Atikokan', 'America/Bahia', 'America/Bahia_Banderas', 
+    'America/Barbados', 'America/Belem', 'America/Belize', 'America/Blanc-Sablon', 'America/Boa_Vista', 
+    'America/Bogota', 'America/Boise', 'America/Cambridge_Bay', 'America/Campo_Grande', 'America/Cancun', 
+    'America/Caracas', 'America/Cayenne', 'America/Cayman', 'America/Chicago', 'America/Chihuahua', 
+    'America/Ciudad_Juarez', 'America/Costa_Rica', 'America/Creston', 'America/Cuiaba', 'America/Curacao', 
+    'America/Danmarkshavn', 'America/Dawson', 'America/Dawson_Creek', 'America/Denver', 'America/Detroit', 
+    'America/Dominica', 'America/Edmonton', 'America/Eirunepe', 'America/El_Salvador', 'America/Fort_Nelson',
+    'America/Fortaleza', 'America/Glace_Bay', 'America/Goose_Bay', 'America/Grand_Turk', 'America/Grenada', 
+    'America/Guadeloupe', 'America/Guatemala', 'America/Guayaquil', 'America/Guyana', 'America/Halifax', 
+    'America/Havana', 'America/Hermosillo', 'America/Indiana/Indianapolis', 'America/Indiana/Knox', 
+    'America/Indiana/Marengo', 'America/Indiana/Petersburg', 'America/Indiana/Tell_City', 'America/Indiana/Vevay', 
+    'America/Indiana/Vincennes', 'America/Indiana/Winamac', 'America/Inuvik', 'America/Iqaluit', 'America/Jamaica', 
+    'America/Juneau', 'America/Kentucky/Louisville', 'America/Kentucky/Monticello', 'America/La_Paz', 'America/Lima',
+    'America/Los_Angeles', 'America/Maceio', 'America/Managua', 'America/Manaus', 'America/Marigot',
+    'America/Martinique', 'America/Matamoros', 'America/Mazatlan', 'America/Menominee', 'America/Merida', 
+    'America/Metlakatla', 'America/Mexico_City', 'America/Miquelon', 'America/Moncton', 'America/Monterrey', 
+    'America/Montevideo', 'America/Montreal', 'America/Montserrat', 'America/Nassau', 'America/New_York', 
+    'America/Nipigon', 'America/Nome', 'America/Noronha', 'America/North_Dakota/Beulah', 'America/North_Dakota/Center',
+    'America/North_Dakota/New_Salem', 'America/Nuuk', 'America/Ojinaga', 'America/Panama', 'America/Pangnirtung',
+    'America/Paramaribo', 'America/Phoenix', 'America/Port_of_Spain', 'America/Port-au-Prince',
+    'America/Porto_Velho', 'America/Puerto_Rico', 'America/Punta_Arenas', 'America/Rainy_River',
+    'America/Rankin_Inlet', 'America/Recife', 'America/Regina', 'America/Resolute', 'America/Rio_Branco',
+    'America/Santarem', 'America/Santiago', 'America/Santo_Domingo', 'America/Sao_Paulo',
+    'America/Scoresbysund', 'America/Sitka', 'America/St_Barthelemy', 'America/St_Johns',
+    'America/St_Kitts', 'America/St_Lucia', 'America/St_Thomas', 'America/St_Vincent',
+    'America/Swift_Current', 'America/Tegucigalpa', 'America/Thule', 'America/Thunder_Bay',
+    'America/Tijuana', 'America/Toronto', 'America/Tortola', 'America/Vancouver', 'America/Whitehorse',
+    'America/Winnipeg', 'America/Yakutat', 'America/Yellowknife',
+    // Europe
+    'Europe/Amsterdam', 'Europe/Andorra', 'Europe/Astrakhan', 'Europe/Athens', 'Europe/Belgrade',
+    'Europe/Berlin', 'Europe/Bratislava', 'Europe/Brussels', 'Europe/Bucharest', 'Europe/Budapest',
+    'Europe/Busingen', 'Europe/Chisinau', 'Europe/Copenhagen', 'Europe/Dublin', 'Europe/Gibraltar',
+    'Europe/Guernsey', 'Europe/Helsinki', 'Europe/Isle_of_Man', 'Europe/Istanbul', 'Europe/Jersey',
+    'Europe/Kaliningrad', 'Europe/Kirov', 'Europe/Kyiv', 'Europe/Lisbon', 'Europe/Ljubljana', 
+    'Europe/London', 'Europe/Luxembourg', 'Europe/Madrid', 'Europe/Malta', 'Europe/Mariehamn', 
+    'Europe/Minsk', 'Europe/Monaco', 'Europe/Moscow', 'Europe/Nicosia', 'Europe/Oslo', 'Europe/Paris', 
+    'Europe/Podgorica', 'Europe/Prague', 'Europe/Riga', 'Europe/Rome', 'Europe/Samara', 'Europe/San_Marino', 
+    'Europe/Sarajevo', 'Europe/Saratov', 'Europe/Simferopol', 'Europe/Skopje', 'Europe/Sofia', 'Europe/Stockholm', 
+    'Europe/Tallinn', 'Europe/Tirane', 'Europe/Ulyanovsk', 'Europe/Uzhgorod', 'Europe/Vaduz', 'Europe/Vatican', 
+    'Europe/Vienna', 'Europe/Vilnius', 'Europe/Volgograd', 'Europe/Warsaw', 'Europe/Zagreb', 'Europe/Zaporozhye',
+    'Europe/Zurich',
+    // Asia
+    'Asia/Aden', 'Asia/Almaty', 'Asia/Amman', 'Asia/Anadyr', 'Asia/Aqtau', 'Asia/Aqtobe',
+    'Asia/Ashgabat', 'Asia/Atyrau', 'Asia/Baghdad', 'Asia/Bahrain', 'Asia/Baku', 'Asia/Bangkok',
+    'Asia/Barnaul', 'Asia/Beirut', 'Asia/Bishkek', 'Asia/Brunei', 'Asia/Chita', 'Asia/Choibalsan',
+    'Asia/Colombo', 'Asia/Damascus', 'Asia/Dhaka', 'Asia/Dili', 'Asia/Dubai', 'Asia/Dushanbe', 
+    'Asia/Famagusta', 'Asia/Gaza', 'Asia/Hebron', 'Asia/Ho_Chi_Minh', 'Asia/Hong_Kong', 'Asia/Hovd', 
+    'Asia/Irkutsk', 'Asia/Jakarta', 'Asia/Jayapura', 'Asia/Jerusalem', 'Asia/Kabul', 'Asia/Kamchatka', 
+    'Asia/Karachi', 'Asia/Kathmandu', 'Asia/Khandyga', 'Asia/Kolkata', 'Asia/Krasnoyarsk', 'Asia/Kuala_Lumpur', 
+    'Asia/Kuching', 'Asia/Kuwait', 'Asia/Macau', 'Asia/Magadan', 'Asia/Makassar', 'Asia/Manila', 'Asia/Muscat', 
+    'Asia/Nicosia', 'Asia/Novokuznetsk', 'Asia/Novosibirsk', 'Asia/Omsk', 'Asia/Oral', 'Asia/Phnom_Penh',
+    'Asia/Pontianak', 'Asia/Pyongyang', 'Asia/Qatar', 'Asia/Qostanay', 'Asia/Qyzylorda', 'Asia/Riyadh', 
+    'Asia/Sakhalin', 'Asia/Samarkand', 'Asia/Seoul', 'Asia/Shanghai', 'Asia/Singapore', 'Asia/Srednekolymsk', 
+    'Asia/Taipei', 'Asia/Tashkent', 'Asia/Tbilisi', 'Asia/Tehran', 'Asia/Thimphu', 'Asia/Tokyo', 'Asia/Tomsk', 
+    'Asia/Ulaanbaatar', 'Asia/Urumqi', 'Asia/Ust-Nera', 'Asia/Vientiane', 'Asia/Vladivostok', 'Asia/Yakutsk', 
+    'Asia/Yangon', 'Asia/Yekaterinburg', 'Asia/Yerevan',
+    // Africa
+    'Africa/Abidjan', 'Africa/Accra', 'Africa/Addis_Ababa', 'Africa/Algiers', 'Africa/Asmara',
+    'Africa/Bamako', 'Africa/Bangui', 'Africa/Banjul', 'Africa/Bissau', 'Africa/Blantyre',
+    'Africa/Brazzaville', 'Africa/Bujumbura', 'Africa/Cairo', 'Africa/Casablanca', 'Africa/Ceuta',
+    'Africa/Conakry', 'Africa/Dakar', 'Africa/Dar_es_Salaam', 'Africa/Djibouti', 'Africa/Douala',
+    'Africa/El_Aaiun', 'Africa/Freetown', 'Africa/Gaborone', 'Africa/Harare', 'Africa/Johannesburg',
+    'Africa/Juba', 'Africa/Kampala', 'Africa/Khartoum', 'Africa/Kigali', 'Africa/Kinshasa',
+    'Africa/Lagos', 'Africa/Libreville', 'Africa/Lome', 'Africa/Luanda', 'Africa/Lubumbashi',
+    'Africa/Lusaka', 'Africa/Malabo', 'Africa/Maputo', 'Africa/Maseru', 'Africa/Mbabane',
+    'Africa/Mogadishu', 'Africa/Monrovia', 'Africa/Nairobi', 'Africa/Ndjamena', 'Africa/Niamey',
+    'Africa/Nouakchott', 'Africa/Ouagadougou', 'Africa/Porto-Novo', 'Africa/Sao_Tome',
+    'Africa/Tripoli', 'Africa/Tunis', 'Africa/Windhoek',
+    // Australia & Oceania
+    'Australia/Adelaide', 'Australia/Brisbane', 'Australia/Broken_Hill', 'Australia/Currie',
+    'Australia/Darwin', 'Australia/Eucla', 'Australia/Hobart', 'Australia/Lindeman',
+    'Australia/Lord_Howe', 'Australia/Melbourne', 'Australia/Perth', 'Australia/Sydney',
+    'Pacific/Apia', 'Pacific/Auckland', 'Pacific/Bougainville', 'Pacific/Chatham', 'Pacific/Chuuk',
+    'Pacific/Easter', 'Pacific/Efate', 'Pacific/Enderbury', 'Pacific/Fakaofo', 'Pacific/Fiji',
+    'Pacific/Funafuti', 'Pacific/Galapagos', 'Pacific/Gambier', 'Pacific/Guadalcanal', 'Pacific/Guam',
+    'Pacific/Honolulu', 'Pacific/Kanton', 'Pacific/Kiritimati', 'Pacific/Kosrae', 'Pacific/Kwajalein', 
+    'Pacific/Majuro', 'Pacific/Marquesas', 'Pacific/Midway', 'Pacific/Nauru', 'Pacific/Niue', 'Pacific/Norfolk',
+    'Pacific/Noumea', 'Pacific/Pago_Pago', 'Pacific/Palau', 'Pacific/Pitcairn', 'Pacific/Pohnpei',
+    'Pacific/Port_Moresby', 'Pacific/Rarotonga', 'Pacific/Saipan', 'Pacific/Tahiti', 'Pacific/Tarawa',
+    'Pacific/Tongatapu', 'Pacific/Wake', 'Pacific/Wallis'
+  ];
 
   browserstackBrowsers = new BehaviorSubject<BrowserstackBrowser[]>([]);
 
@@ -413,7 +508,7 @@ export class EditFeature implements OnInit, OnDestroy {
         '*',
         Validators.compose([
           Validators.required,
-          Validators.pattern('^[0-9,-/*]+$'),
+          Validators.pattern('^[0-9A-Za-z,-/*]+$'),
         ]),
       ],
     });
@@ -558,16 +653,34 @@ export class EditFeature implements OnInit, OnDestroy {
     if (!this.featureForm.value.run_now) return;
 
     try {
-      // parse cron expression
-      let parser = parseExpression(Object.values(expression).join(' '), {
-        utc: true,
+      const cronExpression = Object.values(expression).join(' ');
+      
+      // Determine which timezone to use for parsing and display
+      const displayTimezone = this.timezone === 'browser-timezone' ? this.userTimezone : this.timezone;
+      
+      // Parse the cron expression as if it's in the selected timezone
+      // This gives us the correct times when user enters times in their timezone
+      let parser = parseExpression(cronExpression, {
+        tz: displayTimezone, // Parse in the selected timezone instead of UTC
       });
+      
       // reset errors
       this.parseError.error = false;
       // reset nextRuns arrays
       this.nextRuns = [];
+      
       for (let i = 0; i < 5; i++) {
-        this.nextRuns.push(parser.next().toDate().toLocaleString());
+        const nextDate = parser.next().toDate();
+        // Format the date in the selected timezone for display
+        this.nextRuns.push(nextDate.toLocaleString(undefined, { 
+          timeZone: displayTimezone,
+          year: 'numeric',
+          month: '2-digit', 
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit'
+        }));
       }
     } catch (error) {
       this.nextRuns = [];
@@ -576,6 +689,76 @@ export class EditFeature implements OnInit, OnDestroy {
         msg: error.message,
       };
     }
+  }
+
+  onTimezoneChange(event: any) {
+    // Update the timezone and re-parse the schedule to show updated times
+    this.timezone = event.value;
+    if (this.featureForm.value.run_now) {
+      const { minute, hour, day_month, month, day_week } = this.featureForm.value;
+      this.parseSchedule({ minute, hour, day_month, month, day_week });
+    }
+  }
+
+  getDisplayTimezone(): string {
+    return this.timezone === 'browser-timezone' ? this.userTimezone : this.timezone;
+  }
+
+  getTimezoneOffset(timezone: string): string {
+    try {
+      const now = new Date();
+      const utcTime = now.getTime() + (now.getTimezoneOffset() * 60000);
+      const targetTime = new Date(utcTime + (this.getTimezoneOffsetInMinutes(timezone) * 60000));
+      
+      const offsetMinutes = this.getTimezoneOffsetInMinutes(timezone);
+      const offsetHours = Math.floor(Math.abs(offsetMinutes) / 60);
+      const offsetMins = Math.abs(offsetMinutes) % 60;
+      
+      const sign = offsetMinutes >= 0 ? '+' : '-';
+      const formattedOffset = offsetMins > 0 
+        ? `UTC${sign}${offsetHours}:${offsetMins.toString().padStart(2, '0')}`
+        : `UTC${sign}${offsetHours}`;
+      
+      return formattedOffset;
+    } catch (error) {
+      return 'UTC';
+    }
+  }
+
+  private getTimezoneOffsetInMinutes(timezone: string): number {
+    try {
+      const now = new Date();
+      const utcTime = new Date(now.toLocaleString('en-US', { timeZone: 'UTC' }));
+      const targetTime = new Date(now.toLocaleString('en-US', { timeZone: timezone }));
+      
+      const offsetMs = targetTime.getTime() - utcTime.getTime();
+      return offsetMs / (1000 * 60);
+    } catch (error) {
+      return 0;
+    }
+  }
+
+  /**
+   * Get timezones by continent/region
+   */
+  getTimezonesByContinent(continent: string): string[] {
+    const regex = new RegExp(`^(${continent})`);
+    return this.allTimezones
+      .filter(tz => regex.test(tz))
+      .sort();
+  }
+
+  /**
+   * Get a display name for a timezone
+   */
+  getTimezoneDisplayName(timezone: string): string {
+    // Remove continent prefix and replace underscores with spaces
+    const displayName = timezone
+      .split('/')[1] // Get the part after the continent
+      ?.replace(/_/g, ' ') // Replace underscores with spaces
+      || timezone; // Fallback to original if no slash found
+    
+    return displayName;
   }
 
   changeSchedule({ checked }: MatCheckboxChange) {
@@ -1092,10 +1275,32 @@ export class EditFeature implements OnInit, OnDestroy {
           'month',
           'day_week',
         ];
-        const cron_values = featureInfo.schedule.split(' ');
+        
+        // Use original_cron if available (for timezone-aware schedules), otherwise use schedule
+        const cronToDisplay = featureInfo.original_cron || featureInfo.schedule;
+        const cron_values = cronToDisplay.split(' ');
+        
         for (let i = 0; i < cron_fields.length; i++) {
           this.featureForm.get(cron_fields[i]).setValue(cron_values[i]);
         }
+        
+        // Set the timezone dropdown to the original timezone if available
+        if (featureInfo.original_timezone) {
+          // Use the comprehensive timezone list for validation
+          const availableTimezones = [...this.allTimezones, 'browser-timezone'];
+          
+          if (availableTimezones.includes(featureInfo.original_timezone)) {
+            this.timezone = featureInfo.original_timezone;
+          } else {
+            // Fallback to browser timezone if saved timezone is not in dropdown
+            this.timezone = 'browser-timezone';
+          }
+        } else {
+          // Fallback to browser timezone if no original timezone is stored
+          this.timezone = 'browser-timezone';
+        }
+        // Trigger change detection to ensure the dropdown updates
+        this.cdr.detectChanges();
       }
       // Try to save all possible feature properties in the form using the same property names
       for (const key in featureInfo) {
@@ -1379,15 +1584,26 @@ export class EditFeature implements OnInit, OnDestroy {
       };
       // Construct schedule for sending
       if (fValues.run_now) {
-        dataToSend.schedule = [
+        const cronExpression = [
           fValues.minute,
           fValues.hour,
           fValues.day_month,
           fValues.month,
           fValues.day_week,
         ].join(' ');
+        
+        dataToSend.schedule = cronExpression;
+        
+        // Add timezone information for backend conversion
+        if (this.timezone === 'browser-timezone') {
+          dataToSend.original_timezone = this.userTimezone;
+        } else {
+          dataToSend.original_timezone = this.timezone;
+        }
       } else {
         dataToSend.schedule = '';
+        // Clear timezone info when schedule is disabled
+        dataToSend.original_timezone = null;
       }
 
       // --------------------------------------------
