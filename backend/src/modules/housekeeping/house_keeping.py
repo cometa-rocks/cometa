1bbc96fd98f8d62b1f7f3c40b44b127d71a6cb34
anandkushwaha064 <hello.anand@amvara.de>
2025-06-03 13:34:54 +0530

    Improved housekeeping #6216

--- backend/src/modules/housekeeping/house_keeping.py
+++ backend/src/modules/housekeeping/house_keeping.py
@@ -30,57 +30,64 @@ class HouseKeepingThread(LogCommand, Thread):
         
     # This method will check for video and delete the file if exists
     def __delete_video_file_if_exists(self, result: Feature_result):
-        try:
-            # Delete video
-            if result.video_url is None or result.video_url.strip()=="":
-                return False
+        video_files = []
+        
+        # Delete video
+        if result.video_url is not None and result.video_url.strip()!="":
+            video_files.append(result.video_url.split("/")[-1])
 
-            videoPath = f"{self.video_directory_path}{result.video_url}"
-            if os.path.isfile(videoPath):
-                self.log(f"{videoPath} file exists", spacing=3)
-                os.remove(videoPath)
-                self.log(f"{videoPath} file deleted", spacing=3)
+
+        for mobile in result.mobile:
+            video_url = mobile.get("video_recording", False)
+            
+            if video_url and video_url.strip()!="":
+                video_files.append(video_url.split("/")[-1])
+        
+        
+        for video in video_files:  
+            videoPath = f"{self.video_directory_path}/{video}"
+            try: 
+                if os.path.isfile(videoPath):
+                    self.log(f"{videoPath} file exists", spacing=3)
+                    os.remove(videoPath)
+                    self.log(f"{videoPath} file deleted", spacing=3)
+                    
+                else:
+                    self.log(
+                        f"Video path {videoPath} is defined but file doesn't exist",
+                        type="warning",
+                        spacing=3,
+                    )
+                    self.log(
+                        f"Complete video file path {videoPath}",
+                        type="warning",
+                        spacing=3,
+                    )
                 return True
-            else:
+            except Exception as exception:
                 self.log(
-                    f"Video path {result.video_url} is defined but file doesn't exist",
-                    type="warning",
-                    spacing=3,
-                )
-                self.log(
-                    f"Complete video file path {videoPath}",
-                    type="warning",
+                    f'Exception occurred while deleting video file  "{videoPath}", Error Message: {traceback.format_exc()}',
+                    type="error",
                     spacing=3,
                 )
                 return False
-        
-        except Exception as exception:
-            self.log(
-                f'Exception occurred while deleting video file  "{result.video_url}", Error Message: {traceback.format_exc()}',
-                type="error",
-                spacing=3,
-            )
-            return False
-        
         
     # PDF file name is stored with Feature_results 
     # This change is introduced on 16-07-2024
     def __delete_pdf_file_if_exists(self, result: Feature_result, ):
-        try:
-            pdf_result_file_path = ""
-            # Delete pdf file
-            if result.pdf_result_file_path is None or result.pdf_result_file_path.strip()=="":
-                pdf_result_file_path = f"{result.feature_id.feature_name}-{result.feature_result_id}.pdf"                
-                # return False
-            else:
-                pdf_result_file_path = result.pdf_result_file_path
+        pdf_result_file_path = ""
+        # Delete pdf file
+        if result.pdf_result_file_path is None or result.pdf_result_file_path.strip()=="":
+            pdf_result_file_path = f"{result.feature_id.feature_name}-{result.feature_result_id}.pdf"                
+        else:
+            pdf_result_file_path = result.pdf_result_file_path
 
+        try:
             pdf_file_full_path = f"{self.pdf_report_file_path}/{pdf_result_file_path}"
             if os.path.isfile(pdf_file_full_path):
                 self.log(f"{pdf_file_full_path} file exists", spacing=3)
                 os.remove(pdf_file_full_path)
                 self.log(f"{pdf_file_full_path} file deleted", spacing=3)
-                return True
             else:
                 self.log(
                         f"PDF report file doesn't exist",
@@ -92,11 +99,11 @@ class HouseKeepingThread(LogCommand, Thread):
                     type="warning",
                     spacing=3,
                 )
-                return False
+            return True
             
         except Exception as exception:
             self.log(
-                f'Exception occurred while deleting PDF report file  "{result.video_url}", Error Message: {traceback.format_exc()}',
+                f'Exception occurred while deleting PDF report file  "{pdf_result_file_path}", Error Message: {traceback.format_exc()}',
                 type="error",
                 spacing=3,
             )
@@ -104,29 +111,29 @@ class HouseKeepingThread(LogCommand, Thread):
         
     # This method will check for screenshots and delete the file they exists
     def __delete_screenshot_file_if_exists(self, step_result: Step_result):
-        try:
-            current_screenshot = os.path.join(
+        current_screenshot = os.path.join(
                 self.screenshot_file_path, step_result.screenshot_current
             )
+        try:
 
             if step_result.screenshot_current and os.path.isfile(current_screenshot):
                 os.remove(current_screenshot)
                 self.log(f"{current_screenshot} file deleted", spacing=3)
-                return True
             else:
                 self.log(
                     f"Screenshot path {step_result.screenshot_current} is defined but file doesn't exist",
                     type="warning",
                     spacing=3,
                 )
-                return False
+            return True
 
         except Exception as exception:
             self.log(
-                f'Exception occurred while deleting video file  "{step_result.screenshot_current}", Error Message: {traceback.format_exc()}',
+                f'Exception occurred while deleting video file  "{current_screenshot}", Error Message: {traceback.format_exc()}',
                 type="error",
                 spacing=3,
             )
+            return False
 
     def filter_and_delete_files(self):
         housekeeping_enabled_departments = []
@@ -167,12 +174,16 @@ class HouseKeepingThread(LogCommand, Thread):
                     spacing=1,
                 )
                 for feature_result in feature_results_with_in_department:
+                    house_keeping_done = True
                     self.log(
                         f"Cleaning Feature_Result [ID: {feature_result.feature_result_id}] [FeatureID: {feature_result.feature_id}]",
                         spacing=2,
                     )
-                    self.__delete_video_file_if_exists(feature_result)
-                    self.__delete_pdf_file_if_exists(feature_result)
+                    if not self.__delete_video_file_if_exists(feature_result):
+                        house_keeping_done = False
+                    if not self.__delete_pdf_file_if_exists(feature_result):
+                        house_keeping_done = False
+                        
                     # Clean Step result screen shots
                     step_results = Step_result.objects.filter(
                         feature_result_id=feature_result.feature_result_id
@@ -184,13 +195,18 @@ class HouseKeepingThread(LogCommand, Thread):
 
                     self.log("Cleaning Step_results", spacing=3)
                     for step_result in step_results:
-                        self.__delete_screenshot_file_if_exists(step_result)
-                        pass
-
+                        if not self.__delete_screenshot_file_if_exists(step_result):
+                            house_keeping_done = False
                     self.log(f"Cleaned {len(step_results)} screenshots", spacing=3)
-                    feature_result.house_keeping_done = True
-                    feature_result.save()
 
+                    if house_keeping_done:
+                        feature_result.house_keeping_done = house_keeping_done
+                        feature_result.save()
+                    else:
+                        self.log(
+                            f"Feature_Result [ID: {feature_result.feature_result_id}] [FeatureID: {feature_result.feature_id}] not cleaned, will retry again",
+                            spacing=2,
+                        )
 
             except Exception as exception:
                 self.log(exception)
