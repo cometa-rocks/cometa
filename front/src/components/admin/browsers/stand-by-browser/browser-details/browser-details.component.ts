158eb9716788fb82a4d7df1a8a39ab38e212a245
ankitak012 <ankitakushwaha121314@gmail.com>
2025-07-02 08:37:59 +0000

    A component has been added to create the browser table.

---
+++ front/src/components/admin/browsers/stand-by-browser/browser-details/browser-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cometa-browser-details',
+  templateUrl: './browser-details.component.html',
+  styleUrls: ['./browser-details.component.scss']
+})
+export class BrowserDetailsComponent {
+
+}
