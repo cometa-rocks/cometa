aa4fcfecc8e34de7eb866848b8a8b6cb84cc624f
Redouan <hello.redouan@amvara.de>
2025-08-01 11:33:25 +0200

    Add mobile reference validation in EditFeature component
    - Introduced a new method to validate mobile references in steps before saving.
    - Added error handling to display validation errors in a dialog.
    - Created a SimpleAlertDialog component for showing alerts.
    - Ensured that invalid mobile references prompt user action to correct them.

---
+++ front/src/app/dialogs/simple-alert/simple-alert.component.ts
@@ -0,0 +1,36 @@
+import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
+import {
+  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
+  MatLegacyDialogModule,
+  MatLegacyDialogRef as MatDialogRef,
+} from '@angular/material/legacy-dialog';
+import { MatLegacyButtonModule } from '@angular/material/legacy-button';
+
+export interface SimpleAlertData {
+  title?: string;
+  message?: string;
+}
+
+@Component({
+  selector: 'simple-alert',
+  template: `
+    <h2 mat-dialog-title>{{ data?.title }}</h2>
+    <mat-dialog-content>
+      <span>{{ data?.message }}</span>
+    </mat-dialog-content>
+    <mat-dialog-actions align="end">
+      <button mat-stroked-button color="primary" [mat-dialog-close]="true">
+        OK
+      </button>
+    </mat-dialog-actions>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatLegacyDialogModule, MatLegacyButtonModule],
+})
+export class SimpleAlertDialog {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: SimpleAlertData,
+    private dialogRef: MatDialogRef<SimpleAlertDialog>
+  ) {}
+} 
