c77dd790a102cfb5ff18ce91d35fafa29bb8d6bd
anandkushwaha064 <hello.anand@amvara.de>
2025-08-19 10:56:09 +0200

    Fixing feature files backup path #6680

--- front/src/app/components/feature-history/feature-history.component.ts
+++ front/src/app/components/feature-history/feature-history.component.ts
@@ -60,7 +60,7 @@ export class FeatureHistoryComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.featureId) {
-      this.log.info('Loading feature history for feature ID: ' + this.featureId);
+      this.log.msg('Loading feature history for feature ID: ' + this.featureId);
       this.loadFeatureHistory();
     } else {
     }
