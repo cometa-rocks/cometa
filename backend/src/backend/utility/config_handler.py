ae60374e4548686fdd2e9c6b2727be687c816d72
wiatakillo <wistakito@gmail.com>
2025-09-18 08:51:55 -0700

    updates ollama to 0.5.4 version and removes configuration variable OLLAMA_AI_TLS_SSL_ENABLED - #6784

--- backend/src/backend/utility/config_handler.py
+++ backend/src/backend/utility/config_handler.py
@@ -25,15 +25,9 @@ def get_cometa_redis_port():
     return get_config("REDIS_PORT","6379")
 
 def get_ollama_ai_api_url():
-    ssl_enabled = ConfigurationManager.get_configuration('OLLAMA_AI_TLS_SSL_ENABLED', 'False')=='True'
     host = ConfigurationManager.get_configuration('OLLAMA_AI_HOST', 'ollama.ai')
     port = ConfigurationManager.get_configuration('OLLAMA_AI_PORT', '8002')
-    
-    protocol = 'https' if ssl_enabled else 'http'
-    
-    # host = get_config("OLLAMA_AI_HOST", "ollama.ai.dev") #Change to actual amvara server IP
-    # port = get_config("OLLAMA_AI_PORT", "8002")
-    return f'{protocol}://{host}:{port}/api/chat/'
+    return f'http://{host}:{port}/api/chat/'
 
 # Add any new environment with in this function, if environment information will be used in during test
 def get_all_cometa_environments():
